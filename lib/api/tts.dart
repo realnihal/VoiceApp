2598878bf0a68ffc96c11f8631f95963da5a05dd
Yash Renwa <yash.renwa@gmail.com>
2023-03-28 18:13:38 +0530

    check this out

--- lib/api/tts.dart
+++ lib/api/tts.dart
@@ -1,8 +1,12 @@
 import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
 
+import 'package:audioplayers/audioplayers.dart';
 import 'package:http/http.dart' as http;
 
 void test() async {
+  AudioPlayer player = AudioPlayer();
   const gender = 'male';
   const lang = 'Hindi';
   const text = 'चॉकलेट आइसक्रीम बनाने की विधि बताओ';
@@ -20,7 +24,12 @@ void test() async {
   final response = await http.post(url, headers: headers, body: payload);
 
   final audio = jsonDecode(response.body)['audio'];
-  print(audio);
+  Uint8List decoded = base64.decode(audio);
+  print(decoded);
+
+  // await audioPlayer.play(BytesSource(decoded));
+  // Comment this out to listen to tts.wav
+  await player.play(BytesSource(decoded));
 
   print('done');
 }
